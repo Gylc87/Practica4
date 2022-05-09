193963134800ea572293552517895e53459a8077
Gylc87 <a19110230@ceti.mx>
2022-05-09 18:06:12 -0500

    commit

--- Practica4.py
+++ Practica4.py
@@ -16,14 +16,14 @@ cv2.putText(imagen,texto,(200,250),font,1,(255,255,255),2,cv2.LINE_AA)
 
 img = cv2.imread("img2.jpg")
 img = cv2.resize(img, dsize=(550, 350), interpolation=cv2.INTER_CUBIC)
-cv2.namedWindow("input image")
-cv2.imshow("input image", img)
+cv2.namedWindow("Imagen Original")
+cv2.imshow("Imagen Original", img)
 
 face = img[50:250, 100:450] 
 gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY) 
 backface = cv2.cvtColor(gray,cv2.COLOR_GRAY2BGR)
 img[50:250, 100:450] = backface
-cv2.imshow("face", img)
+cv2.imshow("ROI", img)
 cv2.imshow('imagen',imagen)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
